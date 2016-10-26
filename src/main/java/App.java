acb71fe5e60da0fd76638ea790b35ac9e5960a11
Gary Lai <garylai1990@gmail.com>
2016-10-26 19:49:57 -0400

    Fix conflict

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,23 +7,196 @@ import org.apache.jena.reasoner.ReasonerRegistry;
 import org.apache.jena.vocabulary.RDF;
 import java.io.FileInputStream;
 import java.util.Properties;
+import org.apache.jena.reasoner.ValidityReport;
+import org.apache.jena.datatypes.RDFDatatype;
+import java.text.DecimalFormat;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 
 public class App {
 
+  static InfModel dbpediaInfModel;
+
   public static void main(String[] args) {
     System.out.println("Starting!");
 
+    loadLogger();
+
+    //Validate the model -- not needed right now
+        /*
+        /System.out.println("dbpediaInfModel");
+        validateModel(dbpediaInfModel);
+        System.out.println();
+        */
+
+    //goal: for type(Person) get birthplace,
+    //      if birthplace country == United States
+    //      then find birthplace $state
+    //
+    //      find United States area in sq miles
+    //      find $state in sq miles
+    //
+    //      $Person was born in $birthplace.state
+    //      What percent of the United States by area
+    //      is $state if the United States is $US.areaSqMiles
+    //      and $state is $state.areaSqMiles? Round to the
+    //      nearest whole number percentage.
+    //
+    //      Ans = ( $state.areaSqMiles / %US.areaSqMiles ) * 100
+
+    makeInferenceModel();
+
+    // 0. Person
+    // 1. State
+    // 2. Granular Location (may be null)
+    ArrayList<Resource[]> personStateOriginal = getPersonAndHomeStateUSA(100);
+
+    //printPSO(personStateOriginal);
+
+    //note: takes either "round" or "precise"
+    //      then arg2 is "sqKm"  or "sqMi"
+    double usaArea = getUsaArea("round", "sqMi");
+
+    String precision = "round";
+    String units = "sqMi";
+    for(Resource[] r : personStateOriginal){
+      double stateArea = getStateArea(precision, units, r[1]);
+      printPSONoLoop(r);
+      System.out.println("State Area:    " + stateArea + " " + units + " (" + precision + ")");
+      System.out.println();
+
+    }
+
+
+
+  }
+  public static double getStateArea(String code, String sqWhat, Resource state){
+    //STILL UNDER CONSTRUCTION
+    Property areaTotalProp = dbpediaInfModel.getProperty("http://dbpedia.org/ontology/PopulatedPlace/areaTotal");
+    NodeIterator stateProps = dbpediaInfModel.listObjectsOfProperty(state, areaTotalProp);
+
+
+    double preciseArea = 0.0;
+    double roundedArea = 0.0;
+
+    while (stateProps.hasNext()){
+      RDFNode stateProperty = stateProps.next();
+      Literal stateLiteral = stateProperty.asLiteral();
+
+      RDFDatatype statePropertyDataType = stateLiteral.asLiteral().getDatatype();
+
+      String numAsString = stateLiteral.getString();
+
+      int intPlaces = numAsString.indexOf('.');
+      int decPlaces = numAsString.length() - intPlaces - 1;
+
+      if(decPlaces > 3){
+        preciseArea = Double.parseDouble(numAsString);
+      }
+      else{
+        roundedArea = Double.parseDouble(numAsString);
+      }
+    }
+
+    if(preciseArea == 0.0){
+      preciseArea = roundedArea;
+    }
+
+    if(sqWhat.equals("sqKm")){
+      if(code.equals("round")){
+        return roundedArea;
+      }
+      if(code.equals("precise")){
+        return preciseArea;
+      }
+    }
+
+    if(sqWhat.equals("sqMi")){
+      //double milesInKm = 0.62137119;
+      double usaSurveySqKmToSqMiles = 0.386100614;
+
+      double preciseAreaInMiles = preciseArea * usaSurveySqKmToSqMiles;
+
+      if(code.equals("round")){
+        DecimalFormat df = new DecimalFormat("#.");
+        return Double.parseDouble(df.format(preciseAreaInMiles));
+      }
+      if(code.equals("precise")){
+        return preciseAreaInMiles;
+      }
+    }
+    return 0.0;
+  }
+
+
+  public static void loadLogger(){
     try {
       Properties props = new Properties();
       props.load(new FileInputStream("log4j.properties"));
       PropertyConfigurator.configure(props);
-    } catch (Exception e) {
+    }
+    catch (Exception e) {
+      //System.out.println("Error loading log4j");
+    }
+  }
 
+
+  public static double getUsaArea(String code, String sqWhat){
+    Resource usaResource = dbpediaInfModel.getResource("http://dbpedia.org/resource/United_States");
+    Property areaTotalProp = dbpediaInfModel.getProperty("http://dbpedia.org/ontology/PopulatedPlace/areaTotal");
+    NodeIterator usaProps = dbpediaInfModel.listObjectsOfProperty(usaResource, areaTotalProp);
+
+    double preciseArea = 0.0;
+    double roundedArea = 0.0;
+
+    while (usaProps.hasNext()){
+      RDFNode usaProperty = usaProps.next();
+      Literal usaLiteral = usaProperty.asLiteral();
+      RDFDatatype usaPropertyDataType = usaLiteral.asLiteral().getDatatype();
+
+      String numAsString = usaLiteral.getString();
+
+      int intPlaces = numAsString.indexOf('.');
+      int decPlaces = numAsString.length() - intPlaces - 1;
+
+      if(decPlaces > 3){
+        preciseArea = Double.parseDouble(numAsString);
+      }
+      else{
+        roundedArea = Double.parseDouble(numAsString);
+      }
     }
 
+
+
+    if(sqWhat.equals("sqKm")){
+      if(code.equals("round")){
+        return roundedArea;
+      }
+      if(code.equals("precise")){
+        return preciseArea;
+      }
+    }
+    if(sqWhat.equals("sqMi")){
+      //double milesInKm = 0.62137119;
+      double usaSurveySqKmToSqMiles = 0.386100614;
+
+      double preciseAreaInMiles = preciseArea * usaSurveySqKmToSqMiles;
+
+      if(code.equals("round")){
+        DecimalFormat df = new DecimalFormat("#.");
+        return Double.parseDouble(df.format(preciseAreaInMiles));
+      }
+      if(code.equals("precise")){
+        return preciseAreaInMiles;
+      }
+    }
+    return 0.0;
+  }
+
+  public static void makeInferenceModel(){
     String schemaDirectory = "Database/dbpedia_2014";
     Dataset schemaDataset = TDBFactory.createDataset(schemaDirectory);
     Model dbpediaSchema = schemaDataset.getDefaultModel() ;
@@ -48,185 +221,166 @@ public class App {
     //Build combined inference model
     Reasoner dbpediaReasoner = ReasonerRegistry.getRDFSReasoner();
     dbpediaReasoner = dbpediaReasoner.bindSchema(dbpediaSchema);
-    InfModel dbpediaInfModel = ModelFactory.createInfModel(dbpediaReasoner, dbpediaBaseModel);
-
-
-	//Validate the model
-    /*
-    /System.out.println("dbpediaInfModel");
-    validateModel(dbpediaInfModel);
-    System.out.println();
-    */
-
-
-
-    //Two resource types needed. Keep note of resource-ontology diff
-    //Property country = dbpediaInfModel.getProperty("http://dbpedia.org/ontology/country");
-    //Property birthPlace = dbpediaInfModel.getProperty("http://dbpedia.org/ontology/birthPlace");
-
-    /*
-    Resource abe = dbpediaInfModel.getResource("http://dbpedia.org/resource/Abraham_Lincoln");
-    Resource person = dbpediaInfModel.getResource("http://dbpedia.org/ontology/Person");
-    Resource abe_home;
-
-
-    // If Abraham Lincoln is type Person -> Is Abraham Lincoln a person?
-    if (dbpediaInfModel.contains(abe, RDF.type, person)) {
-      System.out.println("Abraham Lincoln is a person.");
-
-      if (dbpediaInfModel.contains(abe, birthPlace, (RDFNode) null)){
-        NodeIterator born = dbpediaInfModel.listObjectsOfProperty(abe, birthPlace);
-        System.out.println("Abraham Lincoln has a birthPlace:");
-
-        if(born.hasNext()){
-          RDFNode abe_birthplace = born.next();
-          abe_home = abe_birthplace.asResource();
-          System.out.println(abe_home.getURI());
-          NodeIterator countryIn = dbpediaInfModel.listObjectsOfProperty(abe_home, country);
-
-          if(countryIn.hasNext()){
-            RDFNode abeHomeCountry = countryIn.next();
-            Resource abeCountry = abeHomeCountry.asResource();
-            System.out.println("Abraham Lincoln home country:");
-            System.out.println(abeCountry.getURI());
-          }
-        }
-        else{
-          System.out.println("no place.");
-        }
-      }
-    }
-    else {
-      System.out.println("Abraham Lincoln was failed to be recognized as a person");
-    }
-	*/
-
-
-    //goal: for type(Person) get birthplace,
-    //      if birthplace country == United States
-    //			then find birthplace $state
-    //
-    //		find United States area in sq miles
-    //		find $state in sq miles
-    //
-    //		$Person was born in $birthplace.state
-    //		What percent of the United States by area
-    //		is $state if the United States is $US.areaSqMiles
-    //		and $state is $state.areaSqMiles? Round to the
-    //		nearest whole number percentage.
-    //
-    //		Ans = ( $state.areaSqMiles / %US.areaSqMiles ) * 100
-    ArrayList<Resource[]> personStateOriginal = getPersonAndHomeStateUSA(dbpediaInfModel, 100);
-
-    for(Resource[] indiv : personStateOriginal){
-    	if(indiv[2] == null){
-    		System.out.println("Person: " + indiv[0].getURI());
-    		System.out.println("State : " + indiv[1].getURI());
-    	}
-    	else if (indiv[2] != null){
-    		System.out.println("Person:  " + indiv[0].getURI());
-    		System.out.println("Location:" + indiv[2].getURI());
-    		System.out.println("State :  " + indiv[1].getURI());
-    	}
-    }
-
-    System.out.println("done");
+    dbpediaInfModel = ModelFactory.createInfModel(dbpediaReasoner, dbpediaBaseModel);
   }
 
-  public static ArrayList<Resource[]> getPersonAndHomeStateUSA(InfModel dbpediaInfModel, int iterations){
-  	ArrayList<Resource[]> result = new ArrayList<Resource[]>();
+  public static void printPSONoLoop(Resource[] indiv){
+    if(indiv[2] == null){
+      System.out.println("Person:  " + indiv[0].getURI());
+      System.out.println("Location:" + " null" );
+      System.out.println("State:   " +  indiv[1].getURI());
+    }
+    else if (indiv[2] != null){
+      System.out.println("Person:  " + indiv[0].getURI());
+      System.out.println("Location:" + indiv[2].getURI());
+      System.out.println("State :  " + indiv[1].getURI());
+    }
+  }
 
-  	Property country = dbpediaInfModel.getProperty("http://dbpedia.org/ontology/country");
+
+  public static void printPSO(ArrayList<Resource[]> personStateOriginal){
+    for(Resource[] indiv : personStateOriginal){
+      if(indiv[2] == null){
+        System.out.println("Person: " + indiv[0].getURI());
+        System.out.println("Location:" + " null" );
+        System.out.println("State: " +  indiv[1].getURI());
+      }
+      else if (indiv[2] != null){
+        System.out.println("Person:  " + indiv[0].getURI());
+        System.out.println("Location:" + indiv[2].getURI());
+        System.out.println("State :  " + indiv[1].getURI());
+      }
+      System.out.println();
+    }
+  }
+
+  public static ArrayList<Resource[]> getPersonAndHomeStateUSA(int iterations){
+    ArrayList<Resource[]> result = new ArrayList<Resource[]>();
+
     Property birthPlace = dbpediaInfModel.getProperty("http://dbpedia.org/ontology/birthPlace");
-    Property isPartOf = dbpediaInfModel.getProperty("http://dbpedia.org/ontology/isPartOf");
-    Property state = dbpediaInfModel.getProperty("http://dbpedia.org/ontology/state");
-
-   	Resource unitedStates = dbpediaInfModel.getResource("http://dbpedia.org/resource/United_States");
-
-   	ResIterator peopleWithBirthPlace = dbpediaInfModel.listResourcesWithProperty(birthPlace);
-
 
     int count = 0;
+    int missed = 0;
+    ResIterator peopleWithBirthPlace = dbpediaInfModel.listResourcesWithProperty(birthPlace);
+
     while(count < iterations){
-    		Resource[] individial = new Resource[3];
-    		boolean intermediate = false;
-    		Resource homeState = null;
+      Resource currentPerson = peopleWithBirthPlace.nextResource();
+      Resource[] phs = personHomeStateUSA(currentPerson);
 
-    		Resource currentPerson = peopleWithBirthPlace.nextResource();
+      if (phs != null){ result.add(phs); }
+      else{ missed++; }
 
-    		//Look through multiple birth place entries
-    		NodeIterator birthPlaceList = dbpediaInfModel.listObjectsOfProperty(currentPerson, birthPlace);
-    		while (birthPlaceList.hasNext()){
-    			RDFNode currentBirthPlace = birthPlaceList.next();
-    			Resource currentBirthPlaceResource = currentBirthPlace.asResource();
-
-    			//Check if birthplace is within the USA
-    			if(dbpediaInfModel.contains(currentBirthPlaceResource, country, unitedStates)){
-    				//System.out.println(currentPerson + " ");
-    				//System.out.println(currentBirthPlaceResource.getURI());
-
-    				ResIterator stateBirthPlace = dbpediaInfModel.listResourcesWithProperty(state, currentBirthPlaceResource);
-    				if (stateBirthPlace.hasNext()){
-    					homeState = currentBirthPlaceResource;
-    					intermediate = false;
-    					//System.out.println("STATE! (1)");
-    				}
-    				//else
-    				NodeIterator birthPlacePartOfList = dbpediaInfModel.listObjectsOfProperty(currentBirthPlaceResource, isPartOf);
-    				while(birthPlacePartOfList.hasNext()){
-    					RDFNode currentBirthPlacePartOf = birthPlacePartOfList.next();
-    					Resource currentBirthPlacePartOfResource = currentBirthPlacePartOf.asResource();
-
-    					ResIterator statePartOfBirthPlace = dbpediaInfModel.listResourcesWithProperty(state, currentBirthPlacePartOfResource);
-    					if(statePartOfBirthPlace.hasNext()){
-    						//System.out.println(currentBirthPlacePartOfResource.getURI());
-    						homeState = currentBirthPlacePartOfResource;
-    						intermediate = true;
-    						//System.out.println("STATE! (2)");
-    					}
-    				}
-    				//}
-    				if (homeState != null){
-    					individual[0] = currentPerson;
-    					individial[1] = homeState;
-    					if(intermediate){
-    						individial[3] = currentBirthPlaceResource;
-    					}
-    					else{
-    						individial[3] = null;
-    					}
-
-    					result.add(individial);
-    				}
-					//System.out.println();
-				}
-    		}
-    	count++;
+      count++;
     }
     return result;
 
   }
 
-  /*
-  public static void validateModel(InfModel m){
-    ValidityReport validity = m.validate();
-    if (validity.isValid()) {
-        System.out.println("OK, valid.");
-        return;
-    } else {
-        System.out.println("Conflicts");
-        for (Iterator i = validity.getReports(); i.hasNext();){
-            System.out.println(" - " + i.next());
+
+
+  public static Resource[] personHomeStateUSA(Resource currentPerson){
+    ArrayList<Resource[]> options = new ArrayList<Resource[]>();
+
+    Property country = dbpediaInfModel.getProperty("http://dbpedia.org/ontology/country");
+    Property birthPlace = dbpediaInfModel.getProperty("http://dbpedia.org/ontology/birthPlace");
+    Resource unitedStates = dbpediaInfModel.getResource("http://dbpedia.org/resource/United_States");
+
+    NodeIterator birthPlaceList = dbpediaInfModel.listObjectsOfProperty(currentPerson, birthPlace);
+    while (birthPlaceList.hasNext()){
+      Resource currentBirthPlaceResource = birthPlaceList.next().asResource();
+
+      //Check if birthplace is within the USA
+      if(dbpediaInfModel.contains(currentBirthPlaceResource, country, unitedStates)){
+        Resource[] individual = new Resource[3];
+        Resource homeState = null;
+        Resource homeStateFromIntermediate = null;
+
+        if(isStateInUSA(currentBirthPlaceResource)){
+          homeState = currentBirthPlaceResource;
         }
-        return;
+
+        homeStateFromIntermediate = isPartOfStateInUSA(currentBirthPlaceResource);
+
+        if(homeStateFromIntermediate != null){
+          individual[0] = currentPerson;
+          individual[1] = homeStateFromIntermediate;
+          individual[2] = currentBirthPlaceResource;
+          options.add(individual);
+        }
+        else if(homeStateFromIntermediate == null && homeState != null){
+          individual[0] = currentPerson;
+          individual[1] = homeState;
+          individual[2] = null;
+          options.add(individual);
+        }
+      }
+    }
+
+    Resource[] option = findBest(options);
+    return option;
+  }
+
+  public static Resource[] findBest(ArrayList<Resource[]> options){
+    //pretty terrible way to do this im sure but ehhh
+    for(Resource[] inst : options){
+      //has more granular birthplace
+      if(inst[2] != null){
+        return inst;
+      }
+    }
+
+    for(Resource[] inst : options){
+      //only has state
+      if(inst[2] == null){
+        return inst;
+      }
+    }
+    return null;
+  }
+
+  public static Resource isPartOfStateInUSA(Resource location){
+    Resource state;
+    Property isPartOf = dbpediaInfModel.getProperty("http://dbpedia.org/ontology/isPartOf");
+    NodeIterator locationsPartOfState = dbpediaInfModel.listObjectsOfProperty(location, isPartOf);
+
+    while(locationsPartOfState.hasNext()){
+      Resource locationPartOfStateResource = locationsPartOfState.next().asResource();
+
+      if(isStateInUSA(locationPartOfStateResource)){
+        state = locationPartOfStateResource;
+        return state;
+      }
+    }
+    return null;
+  }
+
+
+  public static boolean isStateInUSA(Resource location){
+    Property state = dbpediaInfModel.getProperty("http://dbpedia.org/ontology/state");
+
+    ResIterator findState = dbpediaInfModel.listResourcesWithProperty(state, location);
+
+    if(findState.hasNext()){
+      return true;
+    }
+    else{
+      return false;
     }
   }
 
-  public static void printStatements(Model m, Resource s, Property p, Resource o) {
-    for (StmtIterator i = m.listStatements(s,p,o); i.hasNext(); ) {
-        Statement stmt = i.nextStatement();
-        System.out.println(" - " + PrintUtil.print(stmt));
+
+  public static void validateModel(InfModel m){
+    ValidityReport validity = m.validate();
+    if (validity.isValid()) {
+      System.out.println("OK, valid.");
+      return;
+    } else {
+      System.out.println("Conflicts");
+      for (Iterator i = validity.getReports(); i.hasNext();){
+        System.out.println(" - " + i.next());
+      }
+      return;
     }
   }
-  */
+
 }
