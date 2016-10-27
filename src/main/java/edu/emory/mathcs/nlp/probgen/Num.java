9b4cc2d0d32fb7f19dd51f87fd90145263f80e79
Gary Lai <garylai1990@gmail.com>
2016-10-27 01:31:19 -0400

    refactor num.app

--- src/main/java/edu/emory/mathcs/nlp/probgen/Num.java
+++ src/main/java/edu/emory/mathcs/nlp/probgen/Num.java
@@ -1,60 +1,50 @@
-import org.apache.jena.base.Sys;
-import org.apache.jena.query.Dataset;
+package edu.emory.mathcs.nlp.probgen;
+
 import org.apache.jena.rdf.model.*;
-import org.apache.jena.tdb.TDBFactory;
-import org.apache.log4j.PropertyConfigurator;
-import java.io.FileInputStream;
-import java.util.Properties;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Num {
 
-  public static void main(String[] args) {
-    try {
-      Properties props = new Properties();
-      props.load(new FileInputStream("log4j.properties"));
-      PropertyConfigurator.configure(props);
-    } catch (Exception e) {
+    private Statement stmt;
+    private StmtIterator iter;
+    private RDFNode object;
 
+    public Num () { }
+
+    public Num (Statement stmt) {
+        this.stmt = stmt;
+        this.object = this.stmt.getObject();
     }
 
-    String numDirectory = "Database/specific_mappingbased_properties_en";
-    Dataset numDataset = TDBFactory.createDataset(numDirectory);
-    Model numData = numDataset.getDefaultModel();
+    public Num (StmtIterator iter) {
+        this.iter = iter;
+    }
 
-    StmtIterator iter = numData.listStatements();
-
-    // print out the predicate, subject and object of each statement
-    while (iter.hasNext()) {
-      Statement stmt      = iter.nextStatement();  // get next statement
-      Resource  subject   = stmt.getSubject();     // get the subject
-      Property  predicate = stmt.getPredicate();   // get the predicate
-      RDFNode object    = stmt.getObject();      // get the object
-
-      System.out.print("Resources: ");
-      System.out.print(subject.toString());
-      System.out.print("\n");
-      System.out.print("Property: ");
-      System.out.print(predicate.toString());
-      System.out.print("\n");
-
-      if (object instanceof Resource) {
-
-      } else {
+    public float getNum() {
         String line = object.toString();
         String pattern = "(\\d+.\\d+|\\d+)";
         Pattern r = Pattern.compile(pattern);
         Matcher m = r.matcher(line);
-        if (m.find()) {
-          float f = Float.parseFloat(m.group(0));
-          System.out.println("Found value: " + f );
-          System.out.print("\n");
-        }else {
-          System.out.println("NO MATCH");
+        float f;
+        f = 0;
+        try {
+            m.find();
+            f = Float.parseFloat(m.group(0));
+        } catch (Exception e) {
+            System.out.println("Error:" + e.getMessage());
         }
-      }
-
+        return f;
     }
-}
+
+    public float getSumOfStatementList() {
+        float sum = 0;
+        while (this.iter.hasNext()) {
+            Statement stmt = this.iter.nextStatement();
+            Num nm  = new Num(stmt);
+            sum = sum + nm.getNum();
+        }
+        return sum;
+    }
 }
---
+++ src/test/java/edu/emory/mathcs/nlp/probgen/NumTest.java
@@ -0,0 +1,10 @@
+package edu.emory.mathcs.nlp.probgen;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by gary on 10/26/16.
+ */
+public class NumTest {
+
+}
